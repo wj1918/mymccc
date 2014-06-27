7c211f227b4be280e6b310a99e8a7b16ea40e2ed
Jun Wang <wj1918@hotmail.com>
2014-06-26 22:39:42 -0400

    installing the djangorestframework and the services rest api

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.6.4'],
+    install_requires=['Django>=1.6.4',
+	'djangorestframework'],
 )
--- wsgi/openshift/library_internal/views.py
+++ wsgi/openshift/library_internal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from library_internal.models import CbDb
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from library_internal.serializers import UserSerializer, GroupSerializer
 # Create your views here.
 
 def index(request):
@@ -10,3 +13,19 @@ def report(request, report_name):
     cbdb_list = CbDb.objects.all()[:100]
     context = {'cbdb_list': cbdb_list}
     return render(request, 'report/CB-Label.html', context)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
