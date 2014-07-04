47f5ecc6e0541c205a32b0604c007e54b5e2d060
Jun Wang <wj1918@hotmail.com>
2014-07-04 10:38:44 -0400

    added convert to mysql

--- data/mccc-library-convert.sh
+++ data/mccc-library-convert.sh
@@ -2,15 +2,37 @@
 # This script execute following task
 # 1. download the mdb file from the server
 # 2. convert to sqlite and mysql 
-# 3. upload new new sqlite file to the server
+# 3. upload the new sqlite file to the server
 #
 PROGNAME=$(basename $0)
 
 error_exit()
 {
 
-    echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
-    exit 1
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
+
+convert_to_sqlite3()
+{
+	tables=$(mdb-tables $LIBRARY_MDB)
+	[[ $tables == *CB-DB* ]] || error_exit "Not found LIB_CB-DB table in $LIBRARY_MDB."
+
+	python access-dump.py $LIBRARY_MDB LIB |sqlite3 $LIBRARY_SQLITE_FILE
+
+	cbdb_count=$(sqlite3 $LIBRARY_SQLITE_FILE 'select count(*) from `LIB_CB-DB`')
+
+	[[ cbdb_count -gt 0 ]]  || error_exit "invalid LIB_CB-DB table"
+
+	curl -T $LIBRARY_SQLITE_FILE -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_SQLITE_FILE || error_exit "Failed to upload to ftp server"
+
+}
+
+convert_to_mysql()
+{
+	python access-dump.py $LIBRARY_MDB LIB \
+	| sed '/CREATE TABLE .*/ {N; s/CREATE TABLE .*\n (/&\n\t`id` int(10) NOT NULL auto_increment,\n \t PRIMARY KEY  (`id`), /g}' \
+	| mysql --default-character-set=utf8 mymccc
 }
 
 PATH=$OPENSHIFT_DATA_DIR/usr/bin:$PATH
@@ -22,15 +44,8 @@ curl -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_MDB > $LIB
 
 [[ -f  $LIBRARY_MDB ]] || error_exit "Failed to download $LIBRARY_MDB."
 
-tables=$(mdb-tables $LIBRARY_MDB)
-[[ $tables == *CB-DB* ]] || error_exit "Not found LIB_CB-DB table in $LIBRARY_MDB."
+convert_to_sqlite3
 
-python access-dump.py $LIBRARY_MDB LIB |sqlite3 $LIBRARY_SQLITE_FILE
-
-cbdb_count=$(sqlite3 $LIBRARY_SQLITE_FILE 'select count(*) from `LIB_CB-DB`')
-
-[[ cbdb_count -gt 0 ]]  || error_exit "invalid LIB_CB-DB table"
-
-curl -T $LIBRARY_SQLITE_FILE -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_SQLITE_FILE || error_exit "Failed to upload to ftp server"
+convert_to_mysql
 
 echo "done."
