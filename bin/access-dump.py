521f11bcfdad068268bee4c684da3856376addaf
Jun Wang <wj1918@hotmail.com>
2015-02-23 23:29:30 -0500

    save to sql file

--- bin/access-dump.py
+++ bin/access-dump.py
@@ -11,12 +11,15 @@ DATABASE = sys.argv[1]
 NAMESPACE = sys.argv[2]
  
 # Get the list of table names with "mdb-tables"
-table_names = subprocess.Popen(["mdb-tables", "-1", DATABASE],
-                               stdout=subprocess.PIPE).communicate()[0]
-tables = table_names.splitlines()
-
+#table_names = subprocess.Popen(["mdb-tables", "-1", DATABASE],
+#                               stdout=subprocess.PIPE).communicate()[0]
+#tables = table_names.splitlines()
+tables = ["CB-DB","EB-DB","KEB-DB","CCD-DB","CDVD-DB","EDVD-DB","KEDVD-DB","KCDVD-DB"]
+    
+for table in tables:
+    if table != '':
+        subprocess.call(["mdb-schema", "-T", table, "--drop-table", "-N", NAMESPACE , DATABASE, "mysql"])
 # Dump the schema for the DB
-subprocess.call(["mdb-schema","--drop-table", "-N", NAMESPACE , DATABASE, "mysql"])
 
 print "BEGIN;" # start a transaction, speeds things up when importing
  
--- bin/mccc-library-convert.sh
+++ bin/mccc-library-convert.sh
@@ -24,27 +24,35 @@ convert_to_sqlite3()
 
 	[[ cbdb_count -gt 0 ]]  || error_exit "invalid LIB_CB-DB table"
 
-	curl -T $LIBRARY_SQLITE_FILE -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_SQLITE_FILE || error_exit "Failed to upload to ftp server"
+#	curl -T $LIBRARY_SQLITE_FILE -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_SQLITE_FILE || error_exit "Failed to upload to ftp server"
 
+}
+
+convert_to_mysql_old()
+{
+	python access-dump.py $LIBRARY_MDB LIB \
+	| sed '/CREATE TABLE .*/ {N; s/CREATE TABLE .*\n (/&\n\t`id` int(10) NOT NULL auto_increment,\n \t PRIMARY KEY  (`id`), /g}' \
+	| mysql --default-character-set=utf8 --user=root --password=password mymccc
 }
 
 convert_to_mysql()
 {
-	python access-dump.py $LIBRARY_MDB LIB \
-	| sed '/CREATE TABLE .*/ {N; s/CREATE TABLE .*\n (/&\n\t`id` int(10) NOT NULL auto_increment,\n \t PRIMARY KEY  (`id`), /g}' \
-	| mysql --default-character-set=utf8 mymccc
+	python access-dump.py $LIBRARY_MDB MCCC_Library \
+	| sed '/CREATE TABLE .*/ {N; s/CREATE TABLE .*\n (/&\n\t`id` int(10) NOT NULL auto_increment,\n \t PRIMARY KEY  (`id`), /g}' >mccc-library.sql
+    	
 }
+
 
 PATH=$OPENSHIFT_DATA_DIR/usr/bin:$PATH
 LIBRARY_MDB=MCCC-Library-Access2000.mdb
 LIBRARY_SQLITE_FILE=mccc-library.db
 
-. $OPENSHIFT_DATA_DIR/server.conf
-curl -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_MDB > $LIBRARY_MDB
+# . $OPENSHIFT_DATA_DIR/server.conf
+# curl -u ${FTP_USER}:${FTP_PASSWORD} ftp://${FTP_SERVER}/data/$LIBRARY_MDB > $LIBRARY_MDB
 
 [[ -f  $LIBRARY_MDB ]] || error_exit "Failed to download $LIBRARY_MDB."
 
-convert_to_sqlite3
+#convert_to_sqlite3
 
 convert_to_mysql
 
